e5fc89e95f7a1a23c2baea440d54143d705e60b4
danastasiev <danastasiev@alteryx.com>
2020-09-10 15:28:56 +0300

    add manufacturer ids

--- src/joi/utils.ts
+++ src/joi/utils.ts
@@ -1,7 +1,14 @@
 import { Schema } from 'joi';
 import {HttpError} from "routing-controllers";
 export const validatePayload = (obj: object, schema: Schema) => {
-    const result = schema.validate(obj);
+    const result = schema.validate(obj, {
+        allowUnknown: true,
+        presence: 'required',
+        abortEarly: false,
+        skipFunctions: true,
+        stripUnknown: true,
+        convert: true
+    });
     if(result.error) {
         throw new HttpError(400, result.error.message)
     }
